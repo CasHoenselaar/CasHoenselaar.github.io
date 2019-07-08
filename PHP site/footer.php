3963fcad866df423091d833e12467c9a65f3c703
CasHoenselaar <cashoenselaar@msn.com>
2019-07-08 18:11:29 +0200

    htmll

--- PHP site/footer.php
+++
@@ -1,9 +0,0 @@
-<head>
-  <link rel="stylesheet" type="text/css" href="css/layout.css">
-</head>
-
-<body>
-  <footer id="mainFooter">
-    <p>Copyright &copy; 2019 My website</p>
-  </footer >
-</body>
--- PHP site/includes/dbh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginsystemtut";
-
-$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
